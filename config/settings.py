a25c0c107e4c206859913f9c7486ba13b51e34ff
Aizirek11 <aizireksovetbekova3@gmail.com>
2026-03-08 23:49:14 +0600

    Prepare for GitHub, hide secret key

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cinema-secret-key-2024'
 
-DEBUG = True
 
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
